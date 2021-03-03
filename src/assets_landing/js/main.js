27144fdf5a1739c2eefbc506e3562ea9b604a623
Alexandr Petrovich <apalion97@gmail.com>
2021-03-03 11:42:16 +0200

    commit 03.03 v4

--- src/assets_landing/js/main.js
+++ src/assets_landing/js/main.js
@@ -247,9 +247,9 @@ $(document).ready(function () {
 });
 
 $(document).ready(function(){
-    var youtube_src = $(".modal iframe").attr("src");
+    let youtube_src = $(".modal iframe").attr("src");
     $('.modal').on('show.bs.modal', function () {
-        $(".modal iframe").attr("src", youtube_src + "?autoplay=1");
+        $(".modal iframe").attr("src", youtube_src);
     });
     $(".modal").on('hidden.bs.modal', function (e) {
         $(".modal iframe").attr("src", null);
